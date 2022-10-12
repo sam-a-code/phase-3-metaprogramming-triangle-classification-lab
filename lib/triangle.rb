9b76d1ada002f031cb2d1b744f36de0c68bc57b5
Sam Avery <sam.avery1@gmail.com>
2022-10-12 11:42:47 -0500

    commit tree e1a9afdfb98121617ca9a343834c4253e1d5af52

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end 
+
+  def kind
+    triangle_rules
+    if side_one == side_two && side_two == side_three
+      :equilateral
+    elsif side_one == side_three || side_two == side_three || side_one == side_two
+      :isosceles
+    else
+      :scalene
+    end 
+  end 
+
+  def positive_side?
+    side_one < 0 || side_two < 0 || side_three < 0
+  end 
+
+  def big_side?
+    side_one + side_two < side_three && side_two + side_three < side_one && side_one + side_three < side_two
+  end 
+
+  def zero_side?
+    side_one = 0 || side_two = 0 || side_three = 0
+  end 
+
+  def triangle_rules
+    raise TriangleError unless positive_side? && big_side? && zero_side?
+  end 
+
+# def triangle_rules
+#   if side_one + side_two < side_three
+#     raise TriangleError
+#   elsif side_one = 0 || side_two = 0 || side_three = 0
+#     raise TriangleError
+#   elsif side_one < 0 || side_two < 0 || side_three < 0
+#     raise TriangleError
+#   else 
+#     nil 
+#   end 
+# end 
+
+  class TriangleError < StandardError
+  end 
+end 
